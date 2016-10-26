50e32f1d5fdd028056676e6a94158b278e4980f2
Gina <gdebell@gmail.com>
2016-10-25 21:59:23 -0600

    Added some css style to the main landing page.

--- src/js/components/main/main.controller.js
+++ src/js/components/main/main.controller.js
@@ -3,11 +3,10 @@
   'use strict';
 
   angular
-    .module('myApp.components.main', [])
+    .module('myApp.components.main', ['ui.bootstrap'])
     .controller('mainController', mainController);
 
   mainController.$inject = ['MemberService'];
-
 
   function mainController(MemberService) {
     /*jshint validthis: true */
@@ -17,8 +16,10 @@
       console.log('LOG 3 results from service', resultFromApi);
       this.allMembers = resultFromApi;
       this.oneMember = resultFromApi[0];
+    });
 
-    })
+    this.mainPagePhotos = MemberService.photosArray;
+
   }
 
 })();
--- src/js/components/main/main.services.js
+++ src/js/components/main/main.services.js
@@ -5,17 +5,37 @@
     .module('myApp.services.main', [])
     .service('MemberService', MemberService);
 
-    MemberService.$inject = ['$http'];
+  MemberService.$inject = ['$http'];
 
-    function MemberService ($http) {
+  function MemberService ($http) {
 
-      this.getMembers = function (getInfo) {
-        let gDatesApi= 'https://galvanize-student-apis.herokuapp.com/gdating/members?limit=20&offset=50';
-        return $http.get(gDatesApi)
-        .then((results) => {
-          console.log('LOG 2', results.data.data);
-          return results.data.data;
-        })
+    this.getMembers = function (getInfo) {
+      let gDatesApi = 'https://galvanize-student-apis.herokuapp.com/gdating/members?limit=20&offset=50';
+      return $http.get(gDatesApi)
+      .then((results) => {
+        //console.log('LOG 2', results.data.data);
+        return results.data.data;
+      });
+    };
+
+    this.photosArray = [
+      {
+        image: 'https://unsplash.com/collections/228643/romance?photo=K89617N_IvQ'
+      },
+      {
+        image: 'https://unsplash.com/collections/228643/romance?photo=1otDfVSZnlk'
+      },
+      {
+        image: 'https://images.unsplash.com/photo-1444839368740-f0d3572f8067?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=d86a2a5489d523ab5b15439252795847'
+      },
+      {
+        image: 'https://unsplash.com/search/dating?photo=ihOBrSEdtx8'
+      },
+      {
+        image: 'https://unsplash.com/search/dating?photo=9rHgOVRdrDM'
       }
-    }
+    ]
+
+
+  }
 }());
